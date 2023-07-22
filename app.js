f5f67a9ecea4f5bf5958941e6f12d076ea191d13
lucianovau <lulinovau@gmail.com>
2023-07-22 19:48:52 -0300

    register css

--- app.js
+++ app.js
@@ -5,7 +5,4 @@ const path = require('path');
 app.listen(3000, ()=> 
     console.log('servidor corriendo'));
 
-app.get('/', (req, res)=> {
-    res.sendFile(path.join(__dirname, '/views/index.html'))
-}   );
 
