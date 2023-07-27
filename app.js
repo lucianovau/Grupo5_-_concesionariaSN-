8f079a60b4d877d256edf3f1ed144104277c1581
GraFondato <gracielafondato@gmail.com>
2023-07-27 20:21:35 -0300

    app.js

--- app.js
+++ app.js
@@ -2,6 +2,14 @@ const express = require('express');
 const app = express();
 const path = require('path');
 
+app.use(express.json());
+app.use('/', express.static(__dirname + '/public'));
+
+app.get('/', (req, res) => {
+    let ruta = path.join(__dirname + '/views/home.html');
+    res.sendFile(ruta);
+})
+
 app.listen(3000, ()=> 
     console.log('servidor corriendo'));
 
