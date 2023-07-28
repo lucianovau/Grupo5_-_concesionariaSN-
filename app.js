600ccbda01464b938192009d0003abd4e92ae22f
GraFondato <gracielafondato@gmail.com>
2023-07-28 09:02:18 -0300

    actualizacion de rutas

--- app.js
+++ app.js
@@ -3,11 +3,31 @@ const app = express();
 const path = require('path');
 
 app.use(express.json());
-app.use('/', express.static(__dirname + '/public'));
+app.use( express.static(__dirname + '/public'));
 
 app.get('/', (req, res) => {
     let ruta = path.join(__dirname + '/views/home.html');
     res.sendFile(ruta);
+})
+
+app.get('/register', (req, res) => {
+    res.sendFile(__dirname + '/views/register.html');
+})
+
+app.get('/login', (req, res) => {
+    res.sendFile(__dirname + '/views/login.html');
+})
+
+app.get('/detalleProd', (req, res) => {
+    res.sendFile(__dirname + '/views/detallesProd.html');
+})
+
+app.get('/productCart', (req, res) => {
+    res.sendFile(__dirname + '/views/productCart.html');
+})
+
+app.get('/home', (req, res) => {
+    res.sendFile(__dirname + '/views/home.html');
 })
 
 app.listen(3000, ()=> 
