74e1636b5767a0127704c9eda98277e40e7e385f
lucianovau <lulinovau@gmail.com>
2023-08-10 17:52:58 -0300

    .

--- src/routes/main.js
+++ src/routes/main.js
@@ -7,11 +7,8 @@ router.get('/', controller.home);
 router.get('/home', controller.home);
 router.get('/login', controller.login);
 router.get('/register', controller.registro);
-<<<<<<< HEAD
 router.get('/productCart', controller.cart)
-=======
 router.get('/carga-edicion', controller.cargaEdicion);
->>>>>>> c482744c4a2e4a934d9018573cacc494b3603fc6
 
 
 let productosRutas = require('./productos')
