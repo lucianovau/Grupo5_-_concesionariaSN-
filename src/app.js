be9850b729e41dbea43cbe5fbe36c658cd5e7124
lucianovau <lulinovau@gmail.com>
2023-08-10 17:48:28 -0300

    head y producto

--- src/app.js
+++ src/app.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const app = express();
 const path = require('path');
+const methodOverride = require('method-override')
 
 app.use(express.json());
+app.use(methodOverride('_method'))
 app.use( express.static(path.resolve(__dirname,  '../public')));
 
 app.set('view engine', 'ejs');
--- src/routes/main.js
+++ src/routes/main.js
@@ -7,8 +7,11 @@ router.get('/', controller.home);
 router.get('/home', controller.home);
 router.get('/login', controller.login);
 router.get('/register', controller.registro);
-router.get('/productos', controller.productos);
-router.get('/detallesProd', controller.detalle);
 router.get('/productCart', controller.cart)
+
+
+let productosRutas = require('./productos')
+router.get('/productos', productosRutas);
+router.get('/detalleProd', productosRutas);
 
 module.exports = router
