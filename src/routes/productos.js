62cc0548f5121a2fb634781107ade6a0bdca2ee7
GraFondato <gracielafondato@gmail.com>
2023-09-03 09:35:22 -0300

    multer en form create

--- src/routes/productos.js
+++ src/routes/productos.js
@@ -1,10 +1,26 @@
 const express = require('express');
+const multer = require('multer');
+const path = require('path')
 let router = express.Router();
+
 
 let controllerProductos = require('../controllers/productosController')
 
+const storage = multer.diskStorage({
+    destination: (req, file, callback) => {
+        callback(null, path.join(__dirname, '../public/img/product-img'))
+    },
+    filename: (req, file, callback) => {
+
+        const newFilename = 'prod-' + Date.now + path.extname(file.originalfilename);
+        callback(null, newFilename);
+    }
+});
+
+const upload = multer({ storage });
+
 router.get('/', controllerProductos.productos);
 router.get('/create', controllerProductos.create);
-router.post('/create', controllerProductos.store);
+router.post('/create', upload.single('image'),controllerProductos.store);
 
 module.exports = router
