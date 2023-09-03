75623945ae81eee8263794b142df81817a1c6a55
GraFondato <gracielafondato@gmail.com>
2023-09-03 18:55:52 -0300

    multer form edit

--- src/routes/detalle.js
+++ src/routes/detalle.js
@@ -1,12 +1,28 @@
 const express = require('express');
+const multer = require('multer');
+const path = require('path');
 let router = express.Router();
 
 let controllerProductos = require('../controllers/detalleController')
 
+const storage = multer.diskStorage({
+    destination: (req, file, callback) => {
+        callback(null, path.join(__dirname, '../public/img/product-img'))
+    },
+    filename: (req, file, callback) => {
+
+        const newFilename = 'prod-' + Date.now + path.extname(file.originalfilename);
+        callback(null, newFilename);
+    }
+});
+
+const upload = multer({ storage });
+
+
 router.get('/', controllerProductos.detalleId);
 router.get('/:id', controllerProductos.detalleId);
 router.get('/:id/edit', controllerProductos.edit);
-router.put('/:id/edit', controllerProductos.store);
+router.put('/:id/edit', upload.single('imageUsuario'), controllerProductos.store);
 router.delete('/:id/edit', controllerProductos.delete);
 
 module.exports = router
