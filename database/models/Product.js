6c70cb80447cc2d32975d5b5178dfd9103a088cc
GraFondato <gracielafondato@gmail.com>
2023-10-19 21:18:12 -0300

    modificacion en model-product.js

--- database/models/Product.js
+++ database/models/Product.js
@@ -19,8 +19,28 @@ module.exports = (sequelize, dataTypes) => {
             type: dataTypes.STRING(100),
             allowNull: false,
         },
-        foto: {
-            type: dataTypes.BLOB('long'),
+        img1: {
+            type: dataTypes.STRING(100),
+            allowNull: false,
+        },
+        img2: {
+            type: dataTypes.STRING(100),
+            allowNull: false,
+        },
+        img3: {
+            type: dataTypes.STRING(100),
+            allowNull: false,
+        },
+        img4: {
+            type: dataTypes.STRING(100),
+            allowNull: false,
+        },
+        img5: {
+            type: dataTypes.STRING(100),
+            allowNull: false,
+        },
+        img6: {
+            type: dataTypes.STRING(100),
             allowNull: false,
         },
         descripcion: {
