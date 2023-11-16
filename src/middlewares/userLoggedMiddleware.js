fedc44cb69c4d8412382cf7c996f9b5437c60663
GraFondato <gracielafondato@gmail.com>
2023-11-16 10:14:07 -0300

    userLogged funcional

--- src/middlewares/userLoggedMiddleware.js
+++ src/middlewares/userLoggedMiddleware.js
@@ -1,28 +1,28 @@
-// const User = require('../../database/models/User'); 
+const User = require('../../database/models/User'); 
 
-// async function userLoggedMiddleware(req, res, next) {
-//     res.locals.isLogged = false;
+async function userLoggedMiddleware(req, res, next) {
+    res.locals.isLogged = false;
 
-//     // Verificar si hay un correo electrónico en las cookies
-//     const emailInCookie = req.cookies.userEmail;
+    // Verificar si hay un correo electrónico en las cookies
+    const emailInCookie = req.cookies.userEmail;
 
-//     if (emailInCookie) {
-//         try {
-//             // Intentar encontrar al usuario en la base de datos por correo electrónico
-//             const userFromCookie = await User.findByField('email', emailInCookie);
+    if (emailInCookie) {
+        try {
+            // Intentar encontrar al usuario en la base de datos por correo electrónico
+            const userFromCookie = await User.findByField('email', emailInCookie);
 
-//             if (userFromCookie) {
-//                 // Si se encuentra el usuario, almacenar la información en la sesión
-//                 req.session.userLogged = userFromCookie;
-//                 res.locals.isLogged = true;
-//                 res.locals.userLogged = userFromCookie;
-//             }
-//         } catch (error) {
-//             console.error('Error al buscar al usuario en la base de datos:', error);
-//         }
-//     }
+            if (userFromCookie) {
+                // Si se encuentra el usuario, almacenar la información en la sesión
+                req.session.userLogged = userFromCookie;
+                res.locals.isLogged = true;
+                res.locals.userLogged = userFromCookie;
+            }
+        } catch (error) {
+            console.error('Error al buscar al usuario en la base de datos:', error);
+        }
+    }
 
-//     next();
-// }
+    next();
+}
 
-// module.exports = userLoggedMiddleware;
+module.exports = userLoggedMiddleware;
