9ec83432463d9624171f7f119cbdbe29dd403d6c
lucianovau <lulinovau@gmail.com>
2023-11-17 16:55:35 -0300

    arreglo visuales validaciones del form create y edit products

--- src/routes/detalle.js
+++ src/routes/detalle.js
@@ -5,11 +5,12 @@ let router = express.Router();
 let controllerProductos = require('../controllers/detalleController')
 
 const upload = require('../middlewares/multerProducts')
+const validarProduct = require('../middlewares/validarProduct')
 
 router.get('/', controllerProductos.detalleId);
 router.get('/:id', controllerProductos.detalleId);
 router.get('/:id/edit', controllerProductos.edit);
-router.put('/:id/edit', upload.array('imageProduct', 6), controllerProductos.store);
+router.put('/:id/edit', validarProduct, upload.array('imageProduct', 6), controllerProductos.store);
 router.get('/:id/delete', controllerProductos.delete);
 router.post('/consultar', controllerProductos.consultar)
 router.post('/reservar', controllerProductos.reserve)
